83392626816c2e8074687ef11341e0c03952f25f
Pscodium <peter-larson@hotmail.com>
2024-01-07 23:10:21 -0300

    hotfix/fixing_vite!

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-})
+    plugins: [
+        react(),
+    ],
+    server: {
+        port: 3000,
+        host: 'localhost'
+    },
+    define: {
+        "process.env.BACKEND_ENDPOINT": JSON.stringify(process.env.BACKEND_ENDPOINT),
+    }
+});
